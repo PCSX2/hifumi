1a65ff315096a03aa9868336fff2d909e748479b
pandubz <redpanda4552@gmail.com>
2026-06-08 15:47:30 +0000

    Add a staff notification to the honeypot (#40)

--- src/main/java/net/pcsx2/hifumi/filter/HoneypotHelper.java
+++ src/main/java/net/pcsx2/hifumi/filter/HoneypotHelper.java
@@ -1,21 +1,33 @@
 package net.pcsx2.hifumi.filter;
 
+import java.awt.Color;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
 
+import org.apache.commons.lang3.StringUtils;
+
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.Message.Attachment;
 import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.utils.FileUpload;
+import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
 import net.pcsx2.hifumi.HifumiBot;
 import net.pcsx2.hifumi.moderation.ModActions;
+import net.pcsx2.hifumi.util.AttachmentUtils;
 import net.pcsx2.hifumi.util.RoleUtils;
 
-public class HoneypotFilter implements IFilterHelper {
+public class HoneypotHelper implements IFilterHelper {
 
+    private static final int AGE_MINUTES_TO_REMOVE_MESSAGES = 5;
+    
     private final Message message;
     
-    public HoneypotFilter(Message message) {
+    public HoneypotHelper(Message message) {
         this.message = message;
     }
     
@@ -45,7 +57,7 @@ public class HoneypotFilter implements IFilterHelper {
                 server.addRoleToMember(member, honeypotRole).complete();
                 
                 OffsetDateTime currentTime = OffsetDateTime.now();
-                OffsetDateTime cutoffTime = currentTime.minusMinutes(5);
+                OffsetDateTime cutoffTime = currentTime.minusMinutes(AGE_MINUTES_TO_REMOVE_MESSAGES);
                 ModActions.deleteAllMessageFromUserSinceExcept(member.getIdLong(), cutoffTime.toEpochSecond(), this.message.getIdLong());
                 return true;
             }
@@ -54,11 +66,43 @@ public class HoneypotFilter implements IFilterHelper {
             // Smite them
             ModActions.kickAndNotifyUser(server, member.getIdLong());
             OffsetDateTime currentTime = OffsetDateTime.now();
-            OffsetDateTime cutoffTime = currentTime.minusMinutes(5);
+            OffsetDateTime cutoffTime = currentTime.minusMinutes(AGE_MINUTES_TO_REMOVE_MESSAGES);
             ModActions.deleteAllMessageFromUserSince(member.getIdLong(), cutoffTime.toEpochSecond());
+            this.notifyStaff();
             return true;
         }
         
         return false;
+    }
+    
+    private void notifyStaff() {
+        User user = this.message.getAuthor();
+        
+        EmbedBuilder eb = new EmbedBuilder();
+        eb.setTitle("Automatically fired /spamkick on user in honeypot");
+        eb.setDescription("User has fallen into the honeypot and was automatically kicked after posting a message in another channel.\n\n");
+        eb.appendDescription("Their messages sent within the last " + AGE_MINUTES_TO_REMOVE_MESSAGES + " minutes should be deleted or are in the process of being deleted.\n\n");
+        eb.appendDescription("A short reference of what they sent is included below. No further action is required.\n\n");
+        eb.addField("User ID", user.getId(), true);
+        eb.addField("Username", user.getName(), true);
+        eb.addField("Display Name (as mention)", user.getAsMention(), true);
+        eb.setColor(Color.YELLOW);
+        
+        // Body content preview
+        eb.addField("Body Content (raw, first 100 chars)", StringUtils.abbreviate(this.message.getContentRaw(), 100), false);
+        
+        // Attachments
+        StringBuilder sb = new StringBuilder();
+
+        for (Attachment attachment : this.message.getAttachments()) {
+            sb.append(attachment.getProxyUrl() + "\n");
+        }
+        
+        eb.addField("Attachments", sb.toString(), false);
+
+        ArrayList<FileUpload> files = AttachmentUtils.getMinifiedAttachments(message);
+        MessageCreateBuilder mb = new MessageCreateBuilder();
+        mb.addEmbeds(eb.build());
+        mb.addFiles(files);
     }
 }
